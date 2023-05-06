c98d9e5ead7c5a9c7340179224b11eac6b7c4fe3
RamilA123 <ramilna@code.edu.az>
2023-05-06 08:20:03 +0400

    add folders

---
+++ ASP.NET - FronttoBack/ASP.NET - FronttoBack/Models/Expert.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP.NET___FronttoBack.Models
+{
+    public class Expert
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+}
---
+++ ASP.NET - FronttoBack/ASP.NET - FronttoBack/ViewModels/HomeVm.cs
@@ -0,0 +1,13 @@
+﻿using ASP.NET___FronttoBack.Models;
+
+namespace ASP.NET___FronttoBack.ViewModels
+{
+    public class HomeVm
+    {
+        public List<Service> Services { get; set; }
+        public List<Feature> Features { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Expert> Experts { get; set; }
+        public List<Client> Clients { get; set; }
+    }
+}
